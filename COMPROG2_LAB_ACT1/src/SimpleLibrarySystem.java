77de85d2742eee65122d22349f564415334955da
Exequel <exql.adzn@gmail.com>
2023-02-17 18:56:58 +0800

    add basic flow of program

--- COMPROG2_LAB_ACT1/src/SimpleLibrarySystem.java
+++ COMPROG2_LAB_ACT1/src/SimpleLibrarySystem.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class SimpleLibrarySystem {
     public static void main(String[] args) throws Exception {
 
@@ -8,6 +10,29 @@ public class SimpleLibrarySystem {
         String title3 = "The 48 Laws of Power - Robert Greene";
         String title4 = "How To Win Friends And Influence People - Dale Carnegie";
         String title5 = "Deep Work - Cal Newport";
+
+        while (true) {
+
+            int choice = Integer.parseInt(JOptionPane.showInputDialog(null, "Choose a transaction [1] View [2] Borrow [3] Return: "));
+
+            switch (choice) {
+                // [1] View
+                case 1:
+                    System.out.println("[1] VIEW BOOKS");
+                    break;
+                case 2:
+                // [2] Borrow
+                    System.out.println("[2] BORROW BOOKS");
+                    break;
+                case 3:
+                // [3] Return
+                    System.out.println("[3] RETURN BOOKS");
+                    break;
+                default:
+                    System.out.println("Invalid Option. Please Try Again.");
+            }
+
+        }
 
     }
 }
