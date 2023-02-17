60b24c142b5c4dbb4c4956f08dc6f2dc035cfc50
Exequel <exql.adzn@gmail.com>
2023-02-17 18:47:26 +0800

    add variables for book titles

--- COMPROG2_LAB_ACT1/src/SimpleLibrarySystem.java
+++ COMPROG2_LAB_ACT1/src/SimpleLibrarySystem.java
@@ -1,5 +1,13 @@
 public class SimpleLibrarySystem {
     public static void main(String[] args) throws Exception {
+
         System.out.println("WELCOME to the University Library System!");
+
+        String title1 = "The 4-Hour Workweek - Timothy Ferris";
+        String title2 = "The Millionaire Fastlane - MJ DeMarco";
+        String title3 = "The 48 Laws of Power - Robert Greene";
+        String title4 = "How To Win Friends And Influence People - Dale Carnegie";
+        String title5 = "Deep Work - Cal Newport";
+
     }
 }
