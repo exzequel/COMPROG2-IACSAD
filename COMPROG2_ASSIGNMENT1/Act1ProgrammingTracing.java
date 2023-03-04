ab24dc7e6ccdae9c3d7c893403ea731404185a58
Exequel <exql.adzn@gmail.com>
2023-03-04 12:41:10 +0800

    finish factorial (activity a)

--- COMPROG2_ASSIGNMENT1/Act1ProgrammingTracing.java
+++
@@ -1,7 +0,0 @@
-package COMPROG2_ASSIGNMENT1;
-
-public class Act1ProgrammingTracing {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ COMPROG2_LAB_ACT1/src/Factorial.java
@@ -0,0 +1,30 @@
+// COMPROG2_ASSIGNMENT1 ACTIVITY A
+
+import java.io.*;
+
+public class Factorial {
+
+    public static InputStreamReader reader = new InputStreamReader(System.in);
+    public static BufferedReader input = new BufferedReader(reader);
+
+    public static void main(String[] args) throws Exception {
+        int x;
+        double prod = 1;
+
+        do {
+            System.out.println("Enter a number: ");
+            x = Integer.parseInt(input.readLine());
+
+            if (x < 0 || x > 100) {
+                System.out.println("Invalid!");
+            } else {
+                for (int n = 1; n <= x; n++) {
+                    prod = prod * n;
+                }
+                System.out.println(+prod);
+                prod = 1;
+            }
+
+        } while (x != 0);
+    }
+}
