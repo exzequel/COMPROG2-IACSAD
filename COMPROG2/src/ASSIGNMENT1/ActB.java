058c4eca1d35cff372b16b8ae214193366ddcea0
Exequel <exql.adzn@gmail.com>
2023-03-04 13:07:48 +0800

    add variables

--- COMPROG2/src/ASSIGNMENT1/ActB.java
+++ COMPROG2/src/ASSIGNMENT1/ActB.java
@@ -2,23 +2,29 @@ package ASSIGNMENT1;
 
 public class ActB {
     public static void main(String[] args) {
-        String[][] tv = {
-            {"Program 1: \t", "Program 2: "},
-                {"\t1. gma", "\t1. abs"},
-                {"\t2. qtv", "\t2. cbn"},
-                { "\t3. gma", "\t3. abs" },
-                { "\t4. qtv", "\t4. cbn" },
-                { "\t5. gma", "\t5. abs" },
-                { "\t6. qtv", "\t6. cbn" },
-                { "\t7. gma", "\t7. abs" },
-                { "\t8. qtv", "\t8. cbn" },
-                { "\t9. gma", "\t9. abs" },
-                { "\t10. qtv", "\t10. cbn" }
+        String PROGRAM_1 = "Program 1: \t";
+        String PROGRAM_2 = "Program 2: \t";
+        String GMA = "gma";
+        String QTV = "qtv";
+        String ABS = "abs";
+        String CBN = "cbn";
 
+        String[][] tv = {
+                { PROGRAM_1, PROGRAM_2 },
+                { "\t1. " + GMA + "\t", "\t1. " + ABS },
+                { "\t2. " + QTV + "\t", "\t2. " + CBN },
+                { "\t3. " + GMA + "\t", "\t3. " + ABS },
+                { "\t4. " + QTV + "\t", "\t4. " + CBN },
+                { "\t5. " + GMA + "\t", "\t5. " + ABS },
+                { "\t6. " + QTV + "\t", "\t6. " + CBN },
+                { "\t7. " + GMA + "\t", "\t7. " + ABS },
+                { "\t8. " + QTV + "\t", "\t8. " + CBN },
+                { "\t9. " + GMA + "\t", "\t9. " + ABS },
+                { "\t10. " + QTV, "\t10. " + CBN }
         };
 
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < tv.length; i++) {
+            for (int j = 0; j < tv[i].length; j++) {
                 System.out.print(tv[i][j] + " ");
             }
             System.out.println();
