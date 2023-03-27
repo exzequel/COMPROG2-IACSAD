60dfbd15c5e9c8a2d63a5e7c3bf2844680fdb367
Exequel <exql.adzn@gmail.com>
2023-03-27 19:59:26 +0800

    update Household.java

--- COMPROG2/src/LAB_ACT5/Household.java
+++ COMPROG2/src/LAB_ACT5/Household.java
@@ -1,5 +1,37 @@
 package LAB_ACT5;
 
 public class Household {
-    
+
+    private int occupants;
+    private double income;
+
+    public Household() {
+        this.occupants = 1;
+        this.income = 0.0;
+    }
+
+    public void setOccupants(int occupants) {
+        this.occupants = occupants;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public int getOccupants() {
+        return this.occupants;
+    }
+
+    public double getIncome() {
+        return this.income;
+    }
+
+    public static void main(String[] args) {
+        Household myHousehold = new Household();
+        myHousehold.setOccupants(4);
+        myHousehold.setIncome(20000.0);
+        System.out.println("Occupants: " + myHousehold.getOccupants());
+        System.out.println("Annual Income: " + myHousehold.getIncome());
+
+    }
 }
